4336fbffdfe8a4a7cd773456a446c452c2cc8320
CristianLazoQuispe <mecatronico.lazo@gmail.com>
2022-08-25 20:51:20 +0000

    model

--- spoter/utils.py
+++ spoter/utils.py
@@ -24,7 +24,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
-
+import os
     
 def create_folder(directory):
     if not os.path.exists(directory):
